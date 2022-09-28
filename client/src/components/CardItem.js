ae7fcb055c9ac0b1b5ccca0658c0336a73e82e2c
greystone <greystone@greystone.greystonevn.com>
2022-09-28 09:19:22 +0700

    add node module

--- client/src/components/CardItem.js
+++ client/src/components/CardItem.js
@@ -7,11 +7,7 @@ function CardItem(props) {
       <li className='cards__item'>
         <Link className='cards__item__link' to={props.path}>
           <figure className='cards__item__pic-wrap' data-category={props.label}>
-            <img
-              className='cards__item__img'
-              alt='Travel Image'
-              src={props.src}
-            />
+            <input type="image" img src={props.src} alt="Travel Image" className='cards__item__img' />
           </figure>
           <div className='cards__item__info'>
             <h5 className='cards__item__text'>{props.text}</h5>
--- client/src/components/CardItemTop.js
+++ client/src/components/CardItemTop.js
@@ -8,11 +8,7 @@ function CardItemTop(props) {
         <Link className='cards__item__link' to={props.path}>
           <figure className='cards__item__pic-wrap' data-category={props.label}>
             <a href='#'>
-              <img
-                className='cards__item__img'
-                alt='Travel Image'
-                src={props.src}
-              />
+              <input type="image" img src={props.src} alt="Travel Image" className='cards__item__img' />
             </a>
           </figure>
           <div className='cards__item__info'>
